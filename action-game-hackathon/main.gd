09e06b7f2b5dde22f2db039d1a6b03f0ea676cac
selenicacid <sammyhagenbuch@gmail.com>
2026-04-08 09:09:19 -0400

    Bullet spawning on click

--- action-game-hackathon/main.gd
+++ action-game-hackathon/main.gd
@@ -2,6 +2,13 @@ extends Node
 
 @export var bullet_scene: PackedScene
 
-func physics_process(delta):
+func _physics_process(delta):
 	if Input.is_action_just_pressed("mouse_click"):
+		print("benwquiy")
+		
 		var bullet = bullet_scene.instantiate()
+		
+		bullet.position = $Player.position
+		bullet.linear_velocity = (get_viewport().get_mouse_position() - bullet.position).normalized() * 2000
+		
+		add_child(bullet)
