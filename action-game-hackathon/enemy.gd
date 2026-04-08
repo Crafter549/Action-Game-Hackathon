4251ec034699a21e94647691f060c8df943dd2c3
Crafter549 <Craftero2018@gmail.com>
2026-04-08 08:53:46 -0400

    Enemy script

--- action-game-hackathon/enemy.gd
+++ action-game-hackathon/enemy.gd
@@ -1,5 +1,10 @@
 extends CharacterBody2D
-
+@export var bullet_scene:PackedScene
 
 func _on_shoot_timer_timeout() -> void:
+	var bullet =bullet_scene.instantiate()
+	bullet.position=position
+	bullet.linear_velocity.x=-1000
+	
+	
 	
