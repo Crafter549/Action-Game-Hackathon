a4af29f9b433172ccce04df6e8d4ad94a6f218d2
Crafter549 <Craftero2018@gmail.com>
2026-04-08 09:32:31 -0400

    Bullet direction

--- action-game-hackathon/enemy.gd
+++ action-game-hackathon/enemy.gd
@@ -1,9 +1,11 @@
 extends CharacterBody2D
 @export var bullet_scene:PackedScene
 func _on_shoot_timer_timeout() -> void:
-	var bullet =bullet_scene.instantiate()
-	bullet.linear_velocity.x=-2000s
-	add_child(bullet)
+	var bullet = bullet_scene.instantiate()
+	bullet.position=get_parent().get_child(2).position
+	bullet.linear_velocity=(get_parent().get_child(1).position-bullet.position).normalized()*1000
+	bullet.linear_velocity=bullet.linear_velocity.rotated(randf_range(-PI/6,PI/6))
+	get_parent().add_child(bullet)
 	
 	
 	
