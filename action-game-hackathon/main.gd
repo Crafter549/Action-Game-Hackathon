3b7b89529c20084a81f5ac503baf5a761739e52e
selenicacid <sammyhagenbuch@gmail.com>
2026-04-14 08:58:58 -0400

    Camera tracks mouse

--- action-game-hackathon/main.gd
+++ action-game-hackathon/main.gd
@@ -7,10 +7,10 @@ func _physics_process(delta):
 		var bullet = bullet_scene.instantiate()
 		
 		bullet.position = $Player.position
-		bullet.linear_velocity = (get_viewport().get_mouse_position() + $Camera.position - Vector2(960, 540) - bullet.position + Vector2(0, -100)).normalized() * 1000
+		bullet.linear_velocity = (get_viewport().get_mouse_position() - Vector2(960, 540) + $Camera.position - bullet.position + Vector2(0, -100)).normalized() * 1000
 		
 		bullet.rotation = (bullet.linear_velocity - $Player.velocity).angle()
 		
 		add_child(bullet)
 	
-	$Camera.position += ($Player.position + 0.1 * $Player.velocity - $Camera.position) * 0.05
+	$Camera.position += ($Player.position + 0.15 * $Player.velocity + 0.3 * (get_viewport().get_mouse_position() - Vector2(960, 540)) - $Camera.position) * 0.03
